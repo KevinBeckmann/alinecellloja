3f5788f83f582d9b8419b3a89424c7d806a00916
Kevin Beckmann <kevinbeckmannm@gmail.com>
2026-01-30 16:16:46 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'iplay-admin-v1';
+const urlsToCache = [
+  '/adm.html',
+  '/manifest.json',
+  '/icon-512.png',
+  // Adicione outros assets se precisar, ex: '/style.css' ou bibliotecas
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
